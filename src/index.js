165cf64f26406942397f220eb6f2b29d41a4564e
krishmungase <mungasekrishna8@gmail.com>
2024-07-14 20:22:00 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,46 @@
+const { message } = require("antd");
 const express = require("express");
 const app = express();
+const Joi = require('joi');
 
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send("Welcome To CodeMitra YT - ShareProject")
+})
+
+
+app.post('/api/signup', (req, res) => {
+    const { fullName, email, password, confirmPassword } = req.body;
+
+    const signupSchema = Joi.object({
+        fullName: Joi.string().min(3).required(),
+        email: Joi.string().email().required(),
+        password: Joi.string().min(5).required(),
+        confirmPassword: Joi.ref("password")
+    })
+
+    const { error } = signupSchema.validate(req.body);
+
+    if (error) {
+        return res.status(400).json({
+            success: false,
+            message: "Please check all fields ",
+            error: error.message
+        })
+    }
+
+    if (password !== confirmPassword) {
+        return res.status(400).json({
+            success: false,
+            message: "Password MisMatch"
+        })
+    }
+
+
+    res.status(200).json({
+        message: "data stored"
+    });
 })
 
 app.listen(3000, () => {
